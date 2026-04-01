3eff18a2207ec2dc252671e6edb37992a9264d3e
Rahul Naskar <152627581+wizdomic@users.noreply.github.com>
2026-04-01 10:24:55 +0530

    Update: insert-new-interval - submission-0

--- Data Structures & Algorithms/insert-new-interval/submission-0.py
+++ Data Structures & Algorithms/insert-new-interval/submission-0.py
@@ -1,20 +1,22 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        n = len(intervals)
-        result = []
+        # solve in three way (not overlapping, overlapping, not overlapping)
+        res = []
+        n=len(intervals)
         i=0
 
         while i<n and intervals[i][1] < newInterval[0]:
-            result.append(intervals[i])
+            res.append(intervals[i])
             i+=1
-
+        
         while i<n and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0],intervals[i][0])
             newInterval[1] = max(newInterval[1],intervals[i][1])
             i+=1
-        result.append(newInterval)
+        res.append(newInterval)
 
         while i<n:
-            result.append(intervals[i])
+            res.append(intervals[i])
             i+=1
-        return result
+
+        return res
